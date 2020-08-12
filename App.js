6faf81899603d16fe8dd5a55119b889ccbe9704f
Jonathan Gutierrez <jtgutierrez001@gmail.com>
2020-08-11 21:40:40 -0400

    Add replay at end of video

--- App.js
+++ App.js
@@ -10,89 +10,106 @@ import {
   faUserCircle,
   faComment,
   faShareSquare,
-  faCircle,
 } from "@fortawesome/free-solid-svg-icons";
 
 const cat = Asset.fromModule(require("./videos/cat.mp4")).uri;
 const hockey = Asset.fromModule(require("./videos/hockey.mp4")).uri;
 const shark = Asset.fromModule(require("./videos/shark.mp4")).uri;
-
 let videos = [cat, hockey, shark];
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      0: false,
-      1: false,
-      2: false,
+      videos: [
+        {
+          uri: cat,
+          likes: 356,
+          comments: 110,
+          playing: false,
+        },
+        {
+          uri: hockey,
+          likes: 500,
+          comments: 53,
+          playing: false,
+        },
+        {
+          uri: shark,
+          likes: 1000,
+          comments: 600,
+          playing: false,
+        },
+      ],
     };
+    this.videoRefs = [];
     this.handlePress = this.handlePress.bind(this);
+    this.handleVideoRef = this.handleVideoRef.bind(this);
   }
-  handlePress(j, idx) {
-    this.setState({ [idx]: !this.state[idx] });
+  async handlePress(j, idx) {
+    let videoRef = this.videoRefs[idx];
+    let videoStatus = await videoRef.getStatusAsync();
+
+    if (videoStatus.positionMillis === videoStatus.durationMillis) {
+      await videoRef.replayAsync();
+    } else {
+      this.setState((state) => {
+        state.videos[idx].playing = !state.videos[idx].playing;
+        return { ...state };
+      });
+    }
+  }
+  handleVideoRef(playbackObj, idx) {
+    this.videoRefs.push(playbackObj);
   }
   render() {
     return (
       <ScrollView contentContainerStyle={styles.container}>
-        {videos.map((video, idx) => {
+        {this.state.videos.map((video, idx) => {
           return (
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: "black",
-                zIndex: -1,
-                flexDirection: "column-reverse",
-              }}
-            >
+            <View style={styles.videosContainer} key={idx}>
               <TouchableWithoutFeedback
                 onPress={(j) => this.handlePress(j, idx)}
                 pagingEnabled={true}
                 style={{ zIndex: 0 }}
               >
                 <Video
+                  ref={(playbackObj) => this.handleVideoRef(playbackObj, idx)}
                   source={{
-                    uri: video,
+                    uri: video.uri,
                   }}
                   rate={1.0}
                   volume={1.0}
                   isMuted={false}
                   resizeMode="cover"
-                  shouldPlay={this.state[idx]}
+                  shouldPlay={video.playing}
                   style={{ height: "100%" }}
-                  on
                 />
               </TouchableWithoutFeedback>
-              <View
-                style={{
-                  position: "absolute",
-                  zIndex: 2,
-                  display: "flex",
-                  alignSelf: "flex-end",
-                  justifyContent: "flex-end",
-                  height: "50%",
-                }}
-              >
+              <View style={styles.picContainer}>
                 <FontAwesomeIcon
-                  icon={faCircle}
+                  icon={faUserCircle}
                   style={styles.tinyPic}
                   size={32}
                 />
+
                 <FontAwesomeIcon
                   icon={faHeart}
                   style={styles.tinyPic}
                   size={32}
                 />
+                <Text style={{ ...styles.tinyPicTxt }}>{video.likes}</Text>
                 <FontAwesomeIcon
                   icon={faComment}
-                  style={styles.tinyPic}
+                  style={{ ...styles.tinyPic, color: "#56fc03" }}
                   size={32}
                 />
+                <Text style={{ ...styles.tinyPicTxt }}>{video.comments}</Text>
                 <FontAwesomeIcon
                   icon={faShareSquare}
-                  style={styles.tinyPic}
+                  style={{ ...styles.tinyPic, marginBottom: 25 }}
                   size={32}
-                />
+                ></FontAwesomeIcon>
               </View>
             </View>
           );
@@ -108,10 +125,31 @@ const styles = StyleSheet.create({
     height: `${videos.length}00%`,
     backgroundColor: "#fff",
   },
+  videosContainer: {
+    flex: 1,
+    zIndex: -1,
+    flexDirection: "column-reverse",
+  },
+  picContainer: {
+    position: "absolute",
+    zIndex: 2,
+    display: "flex",
+    alignSelf: "flex-end",
+    justifyContent: "flex-end",
+    height: "50%",
+  },
   tinyPic: {
     zIndex: 1,
     color: "white",
-    margin: 20,
+    marginLeft: 20,
+    marginTop: 20,
     marginRight: 10,
+  },
+  tinyPicTxt: {
+    fontSize: 9,
+    color: "white",
+    textAlign: "right",
+    fontFamily: "Futura",
+    marginRight: 5,
   },
 });
